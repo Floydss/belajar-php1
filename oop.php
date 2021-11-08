24dbbb9427312a0879793c2b61a7c2e99f8228bd
Floydss <theanbou0101@gmail.com>
2021-11-08 21:47:01 +0700

    index-dan-php

--- oop.php
+++ oop.php
@@ -21,4 +21,19 @@ $banana->set_name('Banana');
 echo $apple->get_name();
 echo "<br>";
 echo $banana->get_name();
+
+class Car {
+   // Properties
+   public $name;
+   public $color;
+
+   // Methods
+   function set_name($name) {
+       $this->name = $name;
+   }
+   function get_name() {
+       return $this->name;
+   }
+}
+
 ?>
